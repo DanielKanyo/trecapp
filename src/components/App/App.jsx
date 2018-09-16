b27900dc85dc929b7217f7fb7b4154cdb663b898
DanielKanyo <danielkanyo992@gmail.com>
2018-09-16 21:02:00 +0200

    Not needed landing page removed

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,7 +5,6 @@ import {
 } from 'react-router-dom';
 
 import Navigation from '../Navigation/Navigation';
-import LandingPage from '../Landing/Landing';
 import SignUpPage from '../SignUp/SignUp';
 import SignInPage from '../SignIn/SignIn';
 import PasswordForgetPage from '../PasswordForget/PasswordForget';
@@ -23,7 +22,6 @@ const App = () =>
 
       <hr/>
 
-      <Route exact path={routes.LANDING} component={() => <LandingPage />} />
       <Route exact path={routes.SIGN_UP} component={() => <SignUpPage />} />
       <Route exact path={routes.SIGN_IN} component={() => <SignInPage />} />
       <Route exact path={routes.PASSWORD_FORGET} component={() => <PasswordForgetPage />} />
