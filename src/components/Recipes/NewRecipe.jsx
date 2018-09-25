31b7a744f01263e9068f4fc890c4b3b475a5ff23
DanielKanyo <danielkanyog@gmail.com>
2018-09-25 11:51:14 +0200

    New Recipe component with input fields and checkbox

--- src/components/Recipes/NewRecipe.jsx
+++ src/components/Recipes/NewRecipe.jsx
@@ -5,22 +5,33 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import AddCircle from '@material-ui/icons/AddCircle';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 
 const styles = theme => ({
   paper: {
-    padding: theme.spacing.unit * 2,
     textAlign: 'center',
     color: theme.palette.text.secondary,
     marginBottom: '14px'
-  }
+  },
+  textField: {
+    width: '100%',
+  },
 });
 
 class NewRecipe extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      checked: false,
+    };
   }
+
+  handleChange = name => event => {
+    this.setState({ [name]: event.target.checked });
+  };
 
   render() {
     const { classes } = this.props;
@@ -32,11 +43,47 @@ class NewRecipe extends Component {
           </div>
           <div className="paper-title-text">
             New Recipe
-              </div>
+          </div>
         </Paper>
 
-        <Paper className={classes.paper + ' paper-recipe'}>
-          <p>New Recipe</p>
+        <Paper className={classes.paper + ' paper-recipe-new'}>
+          <TextField
+            id="outlined-name"
+            label="Title"
+            className={classes.textField}
+            margin="normal"
+            placeholder="Recipe title..."
+            variant="outlined"
+          />
+          <TextField
+            id="outlined-name"
+            label="Short description"
+            className={classes.textField}
+            placeholder="Just a few sentences..."
+            margin="normal"
+            variant="outlined"
+          />
+          <TextField
+            id="outlined-multiline-static"
+            label="Long description"
+            multiline
+            rows="4"
+            placeholder="Preparation method..."
+            className={classes.textField}
+            margin="normal"
+            variant="outlined"
+          />
+          <FormControlLabel className="is-recipe-will-be-public-container"
+            control={
+              <Checkbox
+                checked={this.state.checked}
+                onChange={this.handleChange('checked')}
+                value="checked"
+                color="primary"
+              />
+            }
+            label="Public"
+          />
         </Paper>
       </div>
     );
