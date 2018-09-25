0a893b46bd1a9c3964f98f920c2e20c902d48847
DanielKanyo <danielkanyog@gmail.com>
2018-09-25 10:36:08 +0200

    body background changed, beautify code

--- src/components/Recipes/Recipe.jsx
+++ src/components/Recipes/Recipe.jsx
@@ -15,9 +15,8 @@ import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import red from '@material-ui/core/colors/red';
 import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
+import DeleteIcon from '@material-ui/icons/Delete';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 const styles = theme => ({
   paper: {
@@ -77,11 +76,11 @@ class Recipe extends Component {
             avatar={
               <Avatar aria-label="Recipe" className={classes.avatar}>
                 R
-                    </Avatar>
+              </Avatar>
             }
             action={
               <IconButton>
-                <MoreVertIcon />
+                <DeleteIcon />
               </IconButton>
             }
             title="Shrimp and Chorizo Paella"
@@ -96,14 +95,11 @@ class Recipe extends Component {
             <Typography component="p">
               This impressive paella is a perfect party dish and a fun meal to cook together with your
               guests. Add 1 cup of frozen peas along with the mussels, if you like.
-                  </Typography>
+            </Typography>
           </CardContent>
           <CardActions className={classes.actions} disableActionSpacing>
             <IconButton aria-label="Add to favorites">
               <FavoriteIcon />
-            </IconButton>
-            <IconButton aria-label="Share">
-              <ShareIcon />
             </IconButton>
             <IconButton
               className={classnames(classes.expand, {
@@ -120,11 +116,11 @@ class Recipe extends Component {
             <CardContent>
               <Typography paragraph variant="body2">
                 Method:
-                    </Typography>
+              </Typography>
               <Typography paragraph>
                 Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
                 minutes.
-                    </Typography>
+              </Typography>
               <Typography paragraph>
                 Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
                 heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
@@ -132,17 +128,17 @@ class Recipe extends Component {
                 chicken and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion,
                 salt and pepper, and cook, stirring often until thickened and fragrant, about 10
                 minutes. Add saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-                    </Typography>
+              </Typography>
               <Typography paragraph>
                 Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
                 without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat
                 to medium-low, add reserved shrimp and mussels, tucking them down into the rice, and
                 cook again without stirring, until mussels have opened and rice is just tender, 5 to 7
                 minutes more. (Discard any mussels that don’t open.)
-                    </Typography>
+              </Typography>
               <Typography>
                 Set aside off of the heat to let rest for 10 minutes, and then serve.
-                    </Typography>
+              </Typography>
             </CardContent>
           </Collapse>
         </Card>
