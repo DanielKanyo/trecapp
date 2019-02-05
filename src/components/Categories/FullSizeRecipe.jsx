2e70883472957023c9ec7b777addb4dd128e9338
DanielKanyo <danielkanyog@gmail.com>
2019-02-05 10:45:50 +0100

    Loading indicator added to the fullSizeRecipe component, check if recipe exist

--- src/components/Categories/FullSizeRecipe.jsx
+++ src/components/Categories/FullSizeRecipe.jsx
@@ -6,6 +6,7 @@ import withAuthorization from '../Session/withAuthorization';
 import compose from 'recompose/compose';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
+import LinearProgress from '@material-ui/core/LinearProgress';
 
 import Recipe from '../Recipes/Recipe';
 import { ToastContainer } from 'react-toastify';
@@ -20,6 +21,12 @@ const styles = theme => ({
 	chip: {
 		background: '#00925d',
 		color: 'white'
+	},
+	progressLine: {
+		borderRadius: '4px',
+	},
+	progressBar: {
+		background: '#F8B000'
 	}
 });
 
@@ -37,7 +44,8 @@ class FullSizeRecipe extends Component {
 			category: this.props.match.params.category,
 			recipeId: this.props.match.params.id,
 			recipeTitle: 'Recipe Title',
-			recipeCategory: 'Category'
+			recipeCategory: 'Category',
+			loading: true,
 		};
 	}
 
@@ -51,52 +59,59 @@ class FullSizeRecipe extends Component {
 
 			db.getRecipeById(this.props.match.params.id).then(resRecipe => {
 				if (this.mounted) {
-					let recipe = resRecipe.val();
+					if (resRecipe.val()) {
+						let recipe = resRecipe.val();
 
-					let username = usersObject[recipe.userId].username;
-					let profilePicUrl = usersObject[recipe.userId].profilePicUrl;
+						let username = usersObject[recipe.userId].username;
+						let profilePicUrl = usersObject[recipe.userId].profilePicUrl;
 
-					this.setState({
-						loggedInUserId: loggedInUserId
-					});
+						this.setState({
+							loggedInUserId: loggedInUserId
+						});
 
-					let favouritesObject = recipe.favourites;
-					let likesObject = recipe.likes;
+						let favouritesObject = recipe.favourites;
+						let likesObject = recipe.likes;
 
-					let isMine = recipe.userId === loggedInUserId ? true : false;
-					let isFavourite = !favouritesObject ? false : favouritesObject.hasOwnProperty(loggedInUserId) ? true : false;
-					let isLiked = !likesObject ? false : likesObject.hasOwnProperty(loggedInUserId) ? true : false;
-					let recipeDeletable = false;
-					let recipeEditable = isMine;
-					let visibilityEditable = false;
-					let displayUserInfo = true;
-					let withPhoto = recipe.imageUrl !== '' ? true : false;
-					let favouriteCounter = recipe.favouriteCounter;
-					let showMore = true;
+						let isMine = recipe.userId === loggedInUserId ? true : false;
+						let isFavourite = !favouritesObject ? false : favouritesObject.hasOwnProperty(loggedInUserId) ? true : false;
+						let isLiked = !likesObject ? false : likesObject.hasOwnProperty(loggedInUserId) ? true : false;
+						let recipeDeletable = false;
+						let recipeEditable = isMine;
+						let visibilityEditable = false;
+						let displayUserInfo = true;
+						let withPhoto = recipe.imageUrl !== '' ? true : false;
+						let favouriteCounter = recipe.favouriteCounter;
+						let showMore = true;
 
-					let data = recipe;
+						let data = recipe;
 
-					data.loggedInUserId = loggedInUserId;
-					data.username = username;
-					data.profilePicUrl = profilePicUrl;
-					data.isMine = isMine;
-					data.isFavourite = isFavourite;
-					data.isLiked = isLiked;
-					data.favouriteCounter = favouriteCounter;
-					data.recipeDeletable = recipeDeletable;
-					data.recipeEditable = recipeEditable;
-					data.withPhoto = withPhoto;
-					data.visibilityEditable = visibilityEditable;
-					data.displayUserInfo = displayUserInfo;
-					data.showMore = showMore;
-					data.fullSizeRecipe = 'fullSizeRecipe';
-					data.recipeId = this.props.match.params.id;
+						data.loggedInUserId = loggedInUserId;
+						data.username = username;
+						data.profilePicUrl = profilePicUrl;
+						data.isMine = isMine;
+						data.isFavourite = isFavourite;
+						data.isLiked = isLiked;
+						data.favouriteCounter = favouriteCounter;
+						data.recipeDeletable = recipeDeletable;
+						data.recipeEditable = recipeEditable;
+						data.withPhoto = withPhoto;
+						data.visibilityEditable = visibilityEditable;
+						data.displayUserInfo = displayUserInfo;
+						data.showMore = showMore;
+						data.fullSizeRecipe = 'fullSizeRecipe';
+						data.recipeId = this.props.match.params.id;
 
-					let recipeComponent = <Recipe key={data.recipeId} dataProp={data} languageObjectProp={this.props.languageObjectProp} />
+						let recipeComponent = <Recipe key={data.recipeId} dataProp={data} languageObjectProp={this.props.languageObjectProp} />
 
-					this.setState({
-						recipe: recipeComponent
-					});
+						this.setState({
+							recipe: recipeComponent,
+							loading: false,
+						});
+					} else {
+						this.setState({
+							loading: false,
+						});
+					}
 				}
 			});
 		});
@@ -110,11 +125,14 @@ class FullSizeRecipe extends Component {
 	}
 
 	render() {
-
+		const { classes } = this.props;
+		
 		return (
 			<div className="ComponentContent">
 				<Grid className="main-grid" container spacing={16}>
 					<Grid item className="grid-component" xs={12}>
+						{this.state.loading && <LinearProgress classes={{ colorPrimary: classes.progressLine, barColorPrimary: classes.progressBar }} />}
+
 						{this.state.recipe}
 					</Grid>
 				</Grid>
