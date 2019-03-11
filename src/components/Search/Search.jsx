24e97cfebcc9875b12e5dfaf8bf2435bd40870c7
DanielKanyo <danielkanyo992@gmail.com>
2019-03-11 21:00:15 +0100

    Better look for the search field

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -7,39 +7,31 @@ import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import SearchIcon from '@material-ui/icons/Search';
-import Button from '@material-ui/core/Button';
+import InputBase from '@material-ui/core/InputBase';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import TuneIcon from '@material-ui/icons/Tune';
+import ClearIcon from '@material-ui/icons/Clear';
 
 const styles = theme => ({
-  paper: {
+  root: {
+    padding: '6px 4px',
     display: 'flex',
-    justifyContent: 'space-between'
+    alignItems: 'center',
+    width: 'calc(100% - 8px)',
+    marginBottom: 14,
   },
-  search: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    marginLeft: 16,
-    width: 400
+  input: {
+    marginLeft: 8,
+    flex: 1,
   },
-  searchInput: {
-    padding: '10px 16px',
-    borderRadius: 4,
-    border: 'none',
-    background: '#1092ce',
-    color: 'white',
-    fontSize: 14,
-    outline: 'none'
+  iconButton: {
+    padding: 10,
   },
-  button: {
-    color: 'white',
-    marginRight: 10,
-    background: '#1092ce',
-    minWidth: 60
-  },
-  searchButton: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center'
+  divider: {
+    width: 1,
+    height: 28,
+    margin: 4,
   }
 });
 
@@ -52,36 +44,80 @@ class Search extends Component {
 	 */
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      value: '',
+      searchResults: []
+    };
+  }
+
+  /**
+   * Input changed
+   */
+  handleInputChanged = (e) => {
+    this.setState({
+      value: e.target.value
+    });
+  }
+
+  handleSearch = () => {
+    console.log('search', this.state.value);
+  }
+
+  handleToggleTune = () => {
+    console.log('tune');
   }
 
   render() {
     const { classes, languageObjectProp } = this.props;
+    const { searchResults, value } = this.state;
+    const searchButtonAvailable = value.length ? false : true;
 
     return (
       <div className="ComponentContent">
         <Grid className="main-grid" container spacing={16}>
           <Grid item className="grid-component" xs={12}>
-            <Paper className={classes.paper + ' paper-title paper-title-search'}>
-              <div className={classes.search + " search-container"}>
-                <input
-                  className={classes.searchInput + ' search-input'}
-                  type="text"
-                  placeholder={languageObjectProp.data.Search.title}
-                />
-              </div>
-              <div className={classes.searchButton}>
-                <Button variant="contained" className={classes.button + ' search-button'}>
-                  <SearchIcon />
-                </Button>
-              </div>
+            <Paper className={classes.root} elevation={1}>
+              <IconButton 
+                className={classes.iconButton} 
+                aria-label="Tune"
+                onClick={this.handleToggleTune}
+              >
+                <TuneIcon />
+              </IconButton>
+              <InputBase
+                className={classes.input}
+                placeholder={languageObjectProp.data.Search.title}
+                onChange={(e) => { this.handleInputChanged(e) }}
+                value={value}
+              />
+              <IconButton
+                className={classes.iconButton}
+                aria-label="Search"
+                onClick={this.handleSearch}
+                disabled={searchButtonAvailable}
+              >
+                <SearchIcon />
+              </IconButton>
+              <Divider className={classes.divider} />
+              <IconButton className={classes.iconButton} aria-label="Directions">
+                <ClearIcon />
+              </IconButton>
             </Paper>
+
+            <div>
+              {searchResults.length === 0 ? <EmptyList languageObjectProp={languageObjectProp} /> : ''}
+            </div>
           </Grid>
         </Grid>
       </div>
     )
   }
 }
+
+const EmptyList = (props) =>
+  <div className="empty-container">
+    {props.languageObjectProp.data.emptyList}
+  </div>
 
 const authCondition = (authUser) => !!authUser;
 
